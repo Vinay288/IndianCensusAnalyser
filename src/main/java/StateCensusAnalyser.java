29a8228b19a23e928f658b96cebeb3f319e75bd5
Vinay288 <vinayhiremath288@gmail.com>
2021-09-21 22:11:50 +0530

    ADDED: method to load statecode csv

--- src/main/java/StateCensusAnalyser.java
+++ src/main/java/StateCensusAnalyser.java
@@ -9,9 +9,10 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class StateCensusAnalyser {
-    ArrayList<CSVStateCensus> csvList = new ArrayList<>();
+
 
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+        ArrayList<CSVStateCensus> csvList = new ArrayList<>();
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder<CSVStateCensus>(reader)
                     .withType(CSVStateCensus.class)
@@ -19,7 +20,33 @@ public class StateCensusAnalyser {
                     .build();
             Iterator<CSVStateCensus> csvIterator = csvToBean.iterator();
             while (csvIterator.hasNext()) {
-                this.csvList.add(csvIterator.next());
+                csvList.add(csvIterator.next());
+            }
+            return csvList.size();
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+
+        } catch (NullPointerException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES);
+        }
+
+    }
+
+    public int loadStateCodeData(String csvFilePath) throws CensusAnalyserException {
+        ArrayList<CSVStateCode> csvList = new ArrayList<>();
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            CsvToBean<CSVStateCode> csvToBean = new CsvToBeanBuilder<CSVStateCode>(reader)
+                    .withType(CSVStateCode.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CSVStateCode> csvIterator = csvToBean.iterator();
+            while (csvIterator.hasNext()) {
+                csvList.add(csvIterator.next());
             }
             return csvList.size();
         } catch (IOException e) {
