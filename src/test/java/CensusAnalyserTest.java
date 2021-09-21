09748d240cbaea9052152a5267947cfd57e81b83
Vinay288 <vinayhiremath288@gmail.com>
2021-09-21 22:10:15 +0530

    ADDED: tests for load statecode from csv file

--- src/test/java/CensusAnalyserTest.java
+++ src/test/java/CensusAnalyserTest.java
@@ -7,7 +7,7 @@ public class CensusAnalyserTest {
     public void givenStateCensusCSVFile_WhenCorrect_ReturnsCorrectRecords() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/State.csv");
+            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/Census.csv");
             assertEquals(4, numOfRecords);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
@@ -18,7 +18,7 @@ public class CensusAnalyserTest {
     public void givenStateCensusCSVFile_WhenWrongDelimiter_ShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/State_WrongDelimiter.csv");
+            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/Census_WrongDelimiter.csv");
         } catch (CensusAnalyserException e) {
             assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
         }
@@ -28,7 +28,7 @@ public class CensusAnalyserTest {
     public void givenStateCensusCSVFile_WhenMissingHeader_ShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/State_Missing_Header.csv");
+            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/Census_Missing_Header.csv");
         } catch (CensusAnalyserException e) {
             assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
@@ -38,7 +38,7 @@ public class CensusAnalyserTest {
     public void givenStateCensusCSVFile_WhenEmpty_ShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/State_Missing_Header.csv");
+            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/Census_Empty.csv");
         } catch (CensusAnalyserException e) {
             assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
@@ -48,7 +48,58 @@ public class CensusAnalyserTest {
     public void givenStateCensusCSVFile_WhenIncorrectType_ShouldThrowException() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/State_.csv");
+            int numOfRecords = censusAnalyser.loadIndiaCensusData("./src/main/resources/Census_.csv");
+        } catch (CensusAnalyserException e) {
+            assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenStateCodeCSVFile_WhenCorrect_ReturnsCorrectRecords() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadStateCodeData("./src/main/resources/StateCode.csv");
+            assertEquals(4, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenStateCodeCSVFile_WhenWrongDelimiter_ShouldThrowException() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadStateCodeData("./src/main/resources/StateCode_Wrong_Delimiter.csv");
+        } catch (CensusAnalyserException e) {
+            assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+        }
+    }
+
+    @Test
+    public void givenStateCodeCSVFile_WhenMissingHeader_ShouldThrowException() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadStateCodeData("./src/main/resources/StateCode_Missing_Header.csv");
+        } catch (CensusAnalyserException e) {
+            assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+        }
+    }
+
+    @Test
+    public void givenStateCodeCSVFile_WhenEmpty_ShouldThrowException() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadStateCodeData("./src/main/resources/StateCode_Empty.csv");
+        } catch (CensusAnalyserException e) {
+            assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenStateCodeCSVFile_WhenIncorrectType_ShouldThrowException() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadStateCodeData("./src/main/resources/StateCode_.csv");
         } catch (CensusAnalyserException e) {
             assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
